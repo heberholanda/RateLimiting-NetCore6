f5141489d76f536ff1c70378bb7bc393ff6e0e92
Heber Holanda <heberpcl@gmail.com>
2022-10-19 01:01:18 -0300

    Fix QuotaExceededResponse

--- RateLimiting-NetCore6/Program.cs
+++ RateLimiting-NetCore6/Program.cs
@@ -1,4 +1,5 @@
 using AspNetCoreRateLimit;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +13,15 @@ builder.Services.Configure<IpRateLimitOptions>(options =>
 {
     options.EnableEndpointRateLimiting = true;
     options.StackBlockedRequests = false;
-    options.HttpStatusCode = 429;
+    options.HttpStatusCode = (int)HttpStatusCode.TooManyRequests;
     options.RealIpHeader = "X-Real-IP";
     options.QuotaExceededMessage = "Maximum request limit exceeded!";
+    options.QuotaExceededResponse = new QuotaExceededResponse()
+    {
+        Content = "{{ \"message\": \"Whoa! Calm down, cowboy!\", \"details\": \"Quota exceeded. Maximum allowed: {0} per {1}. Please try again in {2} second(s).\" }}",
+        ContentType = "application/json",
+        StatusCode = (int)HttpStatusCode.TooManyRequests
+};
     options.EndpointWhitelist = new List<string>() { "get:/api/license", "*:/api/status" };
     //options.IpWhitelist = new List<string>() { "127.0.0.1", "::1/10", "192.168.0.0/24" };
     options.GeneralRules = new List<RateLimitRule>
@@ -22,7 +29,7 @@ builder.Services.Configure<IpRateLimitOptions>(options =>
             new RateLimitRule
             {
                 Endpoint = "*",
-                Period = "10s",
+                Period = "20s",
                 Limit = 2
             }
         };
