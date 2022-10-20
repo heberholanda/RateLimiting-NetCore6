c124c6e87c1a5ae343c3ff0f7448119f7126b674
Heber Holanda <heberpcl@gmail.com>
2022-10-19 21:40:54 -0300

    Better Configurations

--- RateLimiting-NetCore6/Program.cs
+++ RateLimiting-NetCore6/Program.cs
@@ -1,52 +1,11 @@
-using AspNetCoreRateLimit;
-using System.Net;
+using RateLimiting_NetCore6.ServiceCollection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddMemoryCache();
 
-// Ip / Client Rate Limiting from appsettings.json
-builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
-builder.Services.Configure<ClientRateLimitOptions>(builder.Configuration.GetSection("ClientRateLimiting"));
-
-// Policies
-builder.Services.Configure<IpRateLimitPolicies>(options => builder.Configuration.GetSection("IpRateLimitPolicies").Bind(options));
-builder.Services.Configure<ClientRateLimitPolicies>(options => builder.Configuration.GetSection("ClientRateLimitPolicies").Bind(options));
-
-builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
-builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
-builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
-
-builder.Services.AddInMemoryRateLimiting();
-
-/*
-builder.Services.Configure<IpRateLimitOptions>(options =>
-{
-    options.EnableEndpointRateLimiting = true;
-    options.StackBlockedRequests = false;
-    options.HttpStatusCode = (int)HttpStatusCode.TooManyRequests;
-    options.RealIpHeader = "X-Real-IP";
-    options.QuotaExceededMessage = "Maximum request limit exceeded!";
-    options.QuotaExceededResponse = new QuotaExceededResponse()
-    {
-        Content = "{{ \"message\": \"Whoa! Calm down, cowboy!\", \"details\": \"Quota exceeded. Maximum allowed: {0} per {1}. Please try again in {2} second(s).\" }}",
-        ContentType = "application/json",
-        StatusCode = (int)HttpStatusCode.TooManyRequests
-};
-    options.EndpointWhitelist = new List<string>() { "get:/api/license", "*:/api/status" };
-    //options.IpWhitelist = new List<string>() { "127.0.0.1", "::1/10", "192.168.0.0/24" };
-    options.GeneralRules = new List<RateLimitRule>
-        {
-            new RateLimitRule
-            {
-                Endpoint = "*",
-                Period = "20s",
-                Limit = 2
-            }
-        };
-});
-*/
+// Rate Limiting
+builder.Services.AddRateLimitingServiceCollection(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -62,11 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Ip Rate Limiting
-app.UseIpRateLimiting();
-
-// Client Rate Limiting
-app.UseClientRateLimiting();
+// Rate Limiting
+app.UseRateLimitingServiceCollection();
 
 app.UseHttpsRedirection();
 
---
+++ RateLimiting-NetCore6/ServiceCollection/RateLimitingServiceCollection.cs
@@ -0,0 +1,67 @@
+﻿using AspNetCoreRateLimit;
+using System.Net;
+
+namespace RateLimiting_NetCore6.ServiceCollection
+{
+    internal static class RateLimitingServiceCollection
+    {
+        internal static IServiceCollection AddRateLimitingServiceCollection(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMemoryCache();
+
+            // Ip / Client Rate Limiting from appsettings.json
+            services.Configure<IpRateLimitOptions>(configuration.GetSection("IpRateLimiting"));
+            services.Configure<ClientRateLimitOptions>(configuration.GetSection("ClientRateLimiting"));
+
+            // Policies
+            services.Configure<IpRateLimitPolicies>(options => configuration.GetSection("IpRateLimitPolicies").Bind(options));
+            services.Configure<ClientRateLimitPolicies>(options => configuration.GetSection("ClientRateLimitPolicies").Bind(options));
+
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+            services.AddInMemoryRateLimiting();
+
+            /*
+            services.Configure<IpRateLimitOptions>(options =>
+            {
+                options.EnableEndpointRateLimiting = true;
+                options.StackBlockedRequests = false;
+                options.HttpStatusCode = (int)HttpStatusCode.TooManyRequests;
+                options.RealIpHeader = "X-Real-IP";
+                options.QuotaExceededMessage = "Maximum request limit exceeded!";
+                options.QuotaExceededResponse = new QuotaExceededResponse()
+                {
+                    Content = "{{ \"message\": \"Whoa! Calm down, cowboy!\", \"details\": \"Quota exceeded. Maximum allowed: {0} per {1}. Please try again in {2} second(s).\" }}",
+                    ContentType = "application/json",
+                    StatusCode = (int)HttpStatusCode.TooManyRequests
+            };
+                options.EndpointWhitelist = new List<string>() { "get:/api/license", "*:/api/status" };
+                //options.IpWhitelist = new List<string>() { "127.0.0.1", "::1/10", "192.168.0.0/24" };
+                options.GeneralRules = new List<RateLimitRule>
+                    {
+                        new RateLimitRule
+                        {
+                            Endpoint = "*",
+                            Period = "20s",
+                            Limit = 2
+                        }
+                    };
+            });
+            */
+            return services;
+        }
+
+        internal static IApplicationBuilder UseRateLimitingServiceCollection(this IApplicationBuilder app)
+        {
+            // Ip Rate Limiting
+            app.UseIpRateLimiting();
+
+            // Client Rate Limiting
+            app.UseClientRateLimiting();
+
+            return app;
+        }
+    }
+}
